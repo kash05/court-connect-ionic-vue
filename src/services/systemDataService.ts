23a3a9df5418325e21d4b6f95372cb2122511fa1
kash05 <walkingstatue11@gmail.com>
2025-05-26 09:20:43 +0530

    feat: add sports api

--- src/services/systemDataService.ts
+++ src/services/systemDataService.ts
@@ -1,15 +1,23 @@
 import api from '@/lib/apiClient';
 import { UserRoleInterface } from '@/types/userInterface';
+import { SportsData } from '../types/properyInterface';
 
 export type SystemDataInterface = {
   roles: UserRoleInterface[];
+  sports: SportsData[];
 };
 
 export const SystemData: SystemDataInterface = {
   roles: [],
+  sports: [],
 };
 
 export const getRoles = () =>
   api
     .get<UserRoleInterface[]>('/users/roles')
     .then((res) => (SystemData.roles = res.data));
+
+export const getSports = () =>
+  api
+    .get<SportsData[]>('/sports')
+    .then((res) => (SystemData.sports = res.data));
---
+++ src/types/properyInterface.ts
@@ -0,0 +1,7 @@
+export interface SportsData {
+  id: number;
+  name: string;
+  display_name: string;
+  created_at: string;
+  updated_at: string;
+}
